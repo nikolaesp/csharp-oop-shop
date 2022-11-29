52825d11d40ce6b4a00c0f481f105c3292c6faf7
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-11-29 15:03:17 +0100

    fatoo la classe e il metodi instanziato un ogetto

--- Prodottocs.cs
+++ Prodottocs.cs
@@ -1,13 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharpShop
 {
-    internal class Prodottocs
+    internal class Prodotto
     {
+        private int codice;
+        private string nome;
+        private string descrizione;
+        private int prezzo;
+        private int iva;
 
+
+        public Prodotto(string name,string description,int price,int vat)
+        {
+            Random rnd = new Random();
+            int myRandomNo = rnd.Next(00000000, 99999999); // crea random numero di 8 cifre
+            this.codice = myRandomNo;
+            this.nome = name;
+            this.descrizione = description;
+            this.prezzo = price;
+            this.iva = vat;
+        }
+        public int getCodice() { return this.codice; }
+        public void setNome(string name) {
+
+           this.nome = name;
+        }
+        public string getNome() { return this.nome; }
+        public void setDescrizione(string des)
+        {
+            this.descrizione = des;
+        }
+        public string getDescrizione() { return this.descrizione; }
+        public void setPrezzo(int price)
+        {
+            this.prezzo = price;
+        }
+        public int getPrezzo() { return this.prezzo; }
+        public void setIva(int vat)
+        {
+            this.iva = vat;
+        }
+        public int getIva() { return this.iva; }
+        public void StampaProdotto()
+        {
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Codice: "+this.codice);
+            Console.WriteLine("Descrizione: " + this.descrizione);
+            Console.WriteLine("Prezzo: " + this.prezzo+ "eur");
+            Console.WriteLine("IVA: " + this.iva+"%");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CSharpShop;
+
+Console.WriteLine("Libro1---**---**----**------");
+Prodotto libro1 = new Prodotto("Arthur","Libro science fiction 264 pagine",26,22);
+libro1.StampaProdotto();
+Console.WriteLine("Mistery box////////////////////////");
+Prodotto mysterybox = new Prodotto("Mystery box", "Cartone con ogetti", 120, 22);
+mysterybox.StampaProdotto();
